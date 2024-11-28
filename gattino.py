f1d72e765e5196f15bab169b47cadfcf1aed5895
Salvatore Zappalà <salvatore.zappala@booking.com>
2024-11-28 16:11:13 +0000

    Add "no explanation needed" to the prompt

--- gattino.py
+++ gattino.py
@@ -20,7 +20,7 @@ Explain in plain English the command you want to execute.
     prompt = f"""
 You will be given an action in human language to be executed on a bash shell.
 Please generate a single line bash command that executes the action.
-ATTENTION: please fence the command using a code block
+ATTENTION: please fence the command using a code block. No explanation needed.
 
 Action to execute: {human_language_command}
 """
