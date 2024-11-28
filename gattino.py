73e75c1c638248daeca69a0c132a6e408b018001
Salvatore Zappalà <salvatore.zappala@booking.com>
2024-11-28 16:25:40 +0000

    Do not print prompt

--- gattino.py
+++ gattino.py
@@ -25,8 +25,6 @@ ATTENTION: please fence the command using a code block. No explanation needed. D
 
 Action to execute: {human_language_command}
 """
-    print("PROMPT:")
-    print(prompt)
     with open('/tmp/gattino_prompt.txt', 'w') as file:
         file.write(prompt + '\n')
     model_output = run_command(f'/usr/local/bin/ollama run codellama '' --nowordwrap < /tmp/gattino_prompt.txt')
@@ -49,7 +47,6 @@ def extract_first_code_block(text):
     return ""
 
 def handle_result(args: list[str], answer: str, target_window_id: int, boss: Boss) -> None:
-    # get the kitty window into which to paste answer
     w = boss.window_id_map.get(target_window_id)
     if w is not None:
         w.paste_text(answer)
