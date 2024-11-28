5ac537bd2e30bb13a0c775e19ec5c77fb1e21b5c
Salvatore Zappalà <salvatore.zappala@booking.com>
2024-11-28 13:58:41 +0000

    Add introductory message

--- gattino.py
+++ gattino.py
@@ -3,7 +3,19 @@ from kitty.boss import Boss # type: ignore
 def main(args: list[str]) -> str:
     # this is the main entry point of the kitten, it will be executed in
     # the overlay window when the kitten is launched
-    answer = input('Enter some text: ')
+    print(""" _._     _,-'""`-._
+(,-.`._,'(       |\\`-/|
+    `-.-' \\ )-`( , o o)
+          `-    \\`_`"'-
+
+┌─┐┌─┐┌┬┐┌┬┐┬┌┐┌┌─┐
+│ ┬├─┤ │  │ │││││ │
+└─┘┴ ┴ ┴  ┴ ┴┘└┘└─┘
+
+Explain in plain English the command you want to execute.
+""")
+
+    answer = input('> ')
     # whatever this function returns will be available in the
     # handle_result() function
     return answer
