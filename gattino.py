bdaa4e82dd3957cc821e3d58e25732c9f478acaa
Salvatore Zappalà <salvatore.zappala@booking.com>
2024-11-28 16:23:50 +0000

    Add "do not prefix" to prompt

--- gattino.py
+++ gattino.py
@@ -21,7 +21,7 @@ What do you want to do?
     prompt = f"""
 You will be given an action in human language to be executed on a bash shell.
 Please generate a single line bash command that executes the action.
-ATTENTION: please fence the command using a code block. No explanation needed.
+ATTENTION: please fence the command using a code block. No explanation needed. Do not prefix the line with `$` or `#`.
 
 Action to execute: {human_language_command}
 """
