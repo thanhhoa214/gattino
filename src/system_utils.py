61d6efe01db15c45d69c13cd50361fe5061f16bc
Salvatore Zappalà <salvatore.zappala@booking.com>
2024-12-01 15:25:36 +0000

    Show spinner

--- src/system_utils.py
+++ src/system_utils.py
@@ -1,4 +1,7 @@
 import subprocess
+import sys
+import threading
+import src.ui as ui
 
 
 def write_file(path: str, content: str) -> None:
@@ -8,10 +11,25 @@ def write_file(path: str, content: str) -> None:
 
 
 def run_command(command: str, input: str = None) -> str:
-    process = subprocess.run(
+    process = subprocess.Popen(
         command.split(),
-        input=input,
-        capture_output=True,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
         text=True
     )
-    return process.stdout
+
+    # Start spinner in background thread
+    spinner_thread = threading.Thread(target=ui.show_spinner, args=(process,))
+    spinner_thread.start()
+
+    # Send input and get output
+    stdout, stderr = process.communicate(input=input)
+
+    # Wait for spinner to finish
+    spinner_thread.join()
+
+    if stderr:
+        print(stderr, end='', file=sys.stderr)
+
+    return stdout
--- src/ui.py
+++ src/ui.py
@@ -1,3 +1,27 @@
+import subprocess
+import sys
+import time
+from typing import List
+
+
+class Spinner:
+    def __init__(self):
+        self.frames: List[str] = ['⢄', '⢂', '⢁',
+                                  '⡁', '⡈', '⡐', '⡠']
+        self.index: int = 0
+
+    def next_frame(self) -> str:
+        frame = self.frames[self.index]
+        self.index = (self.index + 1) % len(self.frames)
+        return frame
+
+    def show(self, process: subprocess.Popen) -> None:
+        while process.poll() is None:  # While process is still running
+            print(f"\r{self.next_frame()}", end='', file=sys.stderr)
+            time.sleep(0.1)
+        print("\r", end='', file=sys.stderr)  # Clear the spinner
+
+
 def print_intro() -> None:
     # https://www.asciiart.eu/animals/cats
     # http://patorjk.com/software/taag/#p=display&f=Calvin%20S&t=gattino
@@ -12,6 +36,12 @@ def print_intro() -> None:
 
 """)
 
+
 def print_input_line() -> str:
     print('What do you want to do?')
     return input('> ')
+
+
+def show_spinner(process: subprocess.Popen) -> None:
+    spinner = Spinner()
+    spinner.show(process)
