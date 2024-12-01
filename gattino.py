a98439eab70d9b396e1d49432bc6060516e15475
Salvatore Zappalà <salvatore.zappala@booking.com>
2024-12-01 14:58:49 +0000

    PEP-8

--- gattino.py
+++ gattino.py
@@ -1,9 +1,10 @@
-from kitty.boss import Boss # type: ignore
+from kitty.boss import Boss  # type: ignore
 import src.system_utils as system_utils
 import src.model as model
 import src.config as config
 import src.ui as ui
 import src.parser as parser
+
 
 def main(args: list[str]) -> str:
     config_data = config.load_config()
@@ -12,9 +13,11 @@ def main(args: list[str]) -> str:
     prompt = model.get_prompt(human_language_command)
     system_utils.write_file('/tmp/gattino_prompt.txt', prompt)
     model_name = config_data.get('model', 'codellama')
-    model_output = system_utils.run_command(f'/usr/local/bin/ollama run {model_name} "" --nowordwrap < /tmp/gattino_prompt.txt')
+    model_output = system_utils.run_command(
+        f'/usr/local/bin/ollama run {model_name} "" --nowordwrap < /tmp/gattino_prompt.txt')
     command = parser.extract_first_code_block(model_output)
     return command
+
 
 def handle_result(args: list[str], answer: str, target_window_id: int, boss: Boss) -> None:
     w = boss.window_id_map.get(target_window_id)
