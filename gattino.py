aff1e5d8eb15da4e6a1f1bd91fed07fff5c1065e
Salvatore Zappalà <salvatore.zappala@booking.com>
2024-12-01 16:27:14 +0000

    Move ollama path to config

--- gattino.py
+++ gattino.py
@@ -10,13 +10,15 @@ def main(args: list[str]) -> str:
     config_data = config.load_config()
     ui.print_intro()
     human_language_command = ui.print_input_line()
+
     if not human_language_command:
         return 0
     prompt = model.get_prompt(human_language_command)
-    model_name = config_data.get('model', 'codellama')
 
+    model_name = config_data.get('model', 'codellama')
+    ollama_path = config_data.get('ollama_path', '/usr/local/bin/ollama')
     model_output = system_utils.run_command(
-        f'/usr/local/bin/ollama run {model_name} --nowordwrap',
+        f'{ollama_path} run {model_name} --nowordwrap',
         input=prompt)
     command = parser.extract_first_code_block(model_output)
     return command
